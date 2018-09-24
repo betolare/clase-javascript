e1a90801c8f700f6e94019f27b0a95ce4255201a
betto <betto0680@hotmail.com>
2018-09-24 16:12:56 -0500

    pruebas con variables javascript

--- script/app.js
+++ script/app.js
@@ -1,1 +1,47 @@
-console.log("hola mundo");
+/*
+var num1=10;
+var num2=5;
+var res=num1*num2;
+
+console.log(res);
+
+
+var nombre='betto';
+var profesion="  tecnico";
+console.log(nombre+profesion);
+
+var tiene_mascota=true;
+console.log(tiene_mascota);
+
+
+var datos=["luis",25,false,26,78,"Desarrollador"];
+
+console.log(datos[0]);
+
+estructura if
+var edad=41;
+
+
+if(edad>=18 && edad<=40){
+ console.log("Eres un adulto joven");
+}else {
+	console.log("No eres un adulto joven");
+}
+
+estructura for
+var dias=["Lunes", "Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+
+for(var i=0;i<7;i++){
+	console.log(dias[i]);
+}
+*/
+funcion 
+
+function suma(num1,num2){
+   var resultado=num1+num2;
+   return resultado;
+}
+
+var res=suma(88,55);
+
+console.log(res);
