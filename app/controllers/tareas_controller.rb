ebf9462f1656421770f586c9c3d0196852a4892a
betto <betto0680@hotmail.com>
2018-10-08 16:44:40 -0500

    tema devise para sistema de log y relacion de tablas:usuario y tablas

--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -1,4 +1,6 @@
 class TareasController < ApplicationController
+  before_action :set_tarea, except: [:index, :new, :create]
+  before_action :authenticate_usuario!, except: [:index, :show]
   def index
   	 @tareas = Tarea.all
   	 #select*from tareas
@@ -10,31 +12,46 @@ class TareasController < ApplicationController
    end
 
    def create
-	  @tarea = Tarea.new(titulo: params[:tarea][:titulo], descripcion: params[:tarea][:descripcion])
-	   	
+	  @tarea = Tarea.new(tareas_params) #(titulo: params[:tarea][:titulo], descripcion: params[:tarea][:descripcion])
+	  @tarea.usuario = current_usuario 	
 	  if @tarea.save
 	  #insert into tareas(titulo,descripcion) values(xxx,xxx)
-       redirect_to controller: 'tareas', action: 'show', id: @tarea.id #redirije a la pagina con su id
+       redirect_to @tarea #controller: 'tareas', action: 'show', id: @tarea.id #redirije a la pagina con su id
        else
        	 render :new
 	  end
    end
 
-   def show
-      @tarea = Tarea.find(params[:id])
+   def show      
       #select*from tareas where id=
    end
 
-   def destroy
-      @tarea = Tarea.find(params[:id])
+   def destroy      
       @tarea.destroy
-      redirect_to controller: 'tareas', action: 'index'
+      redirect_to tareas_path#controller: 'tareas', action: 'index'
       #delete*from tareas where id=
    end
 
-   def edit
-   	@tarea = Tarea.find(params[:id])
-   	
+   def edit    	
    end
+
+   def update
+     if @tarea.update(tareas_params)
+      redirect_to @tarea 
+    else
+      render :show
+    end
+   end
+
+  private
+   def set_tarea
+    @tarea = Tarea.find(params[:id])     
+   end
+
+   def tareas_params
+    params.require(:tarea).permit(:titulo, :descripcion)
+     
+   end
+   
 
 end
--- app/models/tarea.rb
+++ app/models/tarea.rb
@@ -1,4 +1,5 @@
 class Tarea < ApplicationRecord
+	 belongs_to :usuario #una tarea va a pertenecer a un usuario
      validates :titulo, presence: true
      validates :descripcion, presence: true
 end
